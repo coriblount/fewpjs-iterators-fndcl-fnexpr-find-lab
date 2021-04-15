5f005805a37f4fadedabad23ba865862785efc02
Cori Blount <heycorihey@icloud.com>
2021-04-15 16:31:57 -0500

    commit tree fb3be287a08af1613260191c9b711b4db065d5e2

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const superbowlWin = (record) => {
+// let result = record.find( record => record.result === "W" )
+//  return !!result ? result.year : undefined
+const result = record.find(superbowl => superbowl.result === "W")
+try {
+    return result.year
+} catch(error) {
+    return result
+}
 
+}
